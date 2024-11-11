7b88088876fc74e7574803172c253b8b2fb842a3
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2024-11-11 18:10:59 -0500

    chore: formatting

--- main.py
+++ main.py
@@ -13,18 +13,23 @@ WOL_INT = os.environ.get("WOL_INT") or ""
 TIMEOUT = int(os.environ.get("TIMEOUT") or "30")
 
 
-if not all((PROXY_TARGET, WOL_MAC, WOL_INT, TIMEOUT )):
+if not all((PROXY_TARGET, WOL_MAC, WOL_INT, TIMEOUT)):
     raise Exception("Environ (one of) [PROXY_URL, WOL_MAC, WOL_INT] is not set.")
 
-def run_etherwake(): 
+
+def run_etherwake():
     subprocess.run(["etherwake", "-i", WOL_INT, WOL_MAC])
+
 
 async def forwardStream(
     request: Request, content: Optional[bytes] = None
 ) -> AsyncGenerator[bytes, Any]:
     client = httpx.AsyncClient(transport=httpx.AsyncHTTPTransport(retries=3))
     async with client.stream(
-        request.method, PROXY_TARGET + request.url.path, content=content, timeout=TIMEOUT, 
+        request.method,
+        PROXY_TARGET + request.url.path,
+        content=content,
+        timeout=TIMEOUT,
     ) as response:
         async for chunk in response.aiter_bytes():
             yield chunk
