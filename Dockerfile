fc091d609f06bb011248ad5fd94a3037c6e629bc
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2024-11-11 18:10:39 -0500

    fix: wol fixes
    - remove wakeonlan
    - add etherwake package to image
    - use subprocess to run etherwake
    - accept interface as arg

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 from python:3.12.7-slim-bullseye
 
 WORKDIR /app
+RUN apt-get update && apt-get install -y etherwake
+
 COPY main.py .
 COPY requirements.txt .
 
--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-from typing import Any, AsyncGenerator, Generator, Optional
+from typing import Any, AsyncGenerator, Optional
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 import httpx
 import os
-from wakeonlan import send_magic_packet
+import subprocess
 
 app = FastAPI()
 
-PROXY_TARGET = os.environ.get("PROXY_URL") or "http://192.168.1.11:11434"
-WOL_MAC = os.environ.get("WOL_MAC") or "B4:2E:99:4D:5C:B4"
-TIMEOUT = os.environ.get("TIMEOUT") or "10"
+PROXY_TARGET = os.environ.get("PROXY_URL") or ""
+WOL_MAC = os.environ.get("WOL_MAC") or ""
+WOL_INT = os.environ.get("WOL_INT") or ""
+TIMEOUT = int(os.environ.get("TIMEOUT") or "30")
 
-if not all((PROXY_TARGET, WOL_MAC, )):
-    raise Exception("Environ (one of) [PROXY_URL, WOL_MAC, WOL_ADDR, WOL_INT] is not set.")
 
+if not all((PROXY_TARGET, WOL_MAC, WOL_INT, TIMEOUT )):
+    raise Exception("Environ (one of) [PROXY_URL, WOL_MAC, WOL_INT] is not set.")
+
+def run_etherwake(): 
+    subprocess.run(["etherwake", "-i", WOL_INT, WOL_MAC])
 
 async def forwardStream(
     request: Request, content: Optional[bytes] = None
 ) -> AsyncGenerator[bytes, Any]:
-    client = httpx.AsyncClient()
+    client = httpx.AsyncClient(transport=httpx.AsyncHTTPTransport(retries=3))
     async with client.stream(
-        request.method, PROXY_TARGET + request.url.path, content=content, timeout=30
+        request.method, PROXY_TARGET + request.url.path, content=content, timeout=TIMEOUT, 
     ) as response:
         async for chunk in response.aiter_bytes():
             yield chunk
@@ -28,13 +32,13 @@ async def forwardStream(
 
 @app.get("/{full_path:path}")
 async def get(request: Request) -> StreamingResponse:
-    send_magic_packet(WOL_MAC,)
+    run_etherwake()
     return StreamingResponse(forwardStream(request), media_type="application/json")
 
 
 @app.post("/{full_path:path}")
 async def post(request: Request) -> StreamingResponse:
-    send_magic_packet(WOL_MAC, )
+    run_etherwake()
     return StreamingResponse(
         forwardStream(request, await request.body()), media_type="application/json"
     )
@@ -42,13 +46,13 @@ async def post(request: Request) -> StreamingResponse:
 
 @app.delete("/{full_path:path}")
 async def delete(request: Request) -> StreamingResponse:
-    send_magic_packet(WOL_MAC, )
+    run_etherwake()
     return StreamingResponse(forwardStream(request), media_type="application/json")
 
 
 @app.put("/{full_path:path}")
 async def put(request: Request) -> StreamingResponse:
-    send_magic_packet(WOL_MAC, )
+    run_etherwake()
     return StreamingResponse(
         forwardStream(request, await request.body()), media_type="application/json"
     )
