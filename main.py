eebd3157c4b2107cf61999ee354b68b15a204d17
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2024-11-11 15:33:03 -0500

    fix: types + formatting + mypy + black

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Any, AsyncGenerator, Generator, Optional
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 import httpx
@@ -11,24 +11,37 @@ PROXY_TARGET = os.environ.get("PROXY_URL") or ""
 if not PROXY_TARGET:
     raise Exception("Environ [PROXY_URL] is not set.")
 
-async def forwardStream(request: Request, content: Optional[bytes] = None): 
+
+async def forwardStream(
+    request: Request, content: Optional[bytes] = None
+) -> AsyncGenerator[bytes, Any]:
     client = httpx.AsyncClient()
-    async with client.stream(request.method, PROXY_TARGET + request.url.path, content=content) as response:
+    async with client.stream(
+        request.method, PROXY_TARGET + request.url.path, content=content
+    ) as response:
         async for chunk in response.aiter_bytes():
             yield chunk
 
+
 @app.get("/{full_path:path}")
-async def get(request: Request):
-    return StreamingResponse(forwardStream(request), media_type='application/json')
+async def get(request: Request) -> StreamingResponse:
+    return StreamingResponse(forwardStream(request), media_type="application/json")
+
 
 @app.post("/{full_path:path}")
-async def post(request: Request):
-    return StreamingResponse(forwardStream(request, await request.body()), media_type='application/json')
+async def post(request: Request) -> StreamingResponse:
+    return StreamingResponse(
+        forwardStream(request, await request.body()), media_type="application/json"
+    )
+
 
 @app.delete("/{full_path:path}")
-async def delete(request: Request):
-    return StreamingResponse(forwardStream(request), media_type='application/json')
+async def delete(request: Request) -> StreamingResponse:
+    return StreamingResponse(forwardStream(request), media_type="application/json")
+
 
 @app.put("/{full_path:path}")
-async def put(request: Request):
-    return StreamingResponse(forwardStream(request, await request.body()), media_type='application/json')
+async def put(request: Request) -> StreamingResponse:
+    return StreamingResponse(
+        forwardStream(request, await request.body()), media_type="application/json"
+    )
